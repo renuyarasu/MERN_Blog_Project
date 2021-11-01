137dfd9226d008b8f246f15ae1fe1ba9f91e299c
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-01 15:35:13 +0530

    Router Implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// Components
 import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticleList from "./pages/ArticleList";
+import Article from "./pages/Article";
+
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
-     <Home />
-    </div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/article-list" component={ArticleList} />
+        <Route exact path="/article" component={Article} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="mb-20">
+        <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">About my Blog</h1>
+        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
+        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
+        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
+        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
+    </div>
+    )
+}
+
+export default About
