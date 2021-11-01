f2ed6f0d9a86ad80358d879cdca03ab000bd6ddb
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-01 17:37:30 +0530

    URL Parameters

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
-function Article() {
+const Article = ({match}) => {
+    const name = match.params.name;
     return (
         <div className="mb-20">
-        <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">Article 01</h1>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-    </div>
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">{name} Article</h1>
+        </div>
     )
 }
 
