17486c2fe9c9fe3ba14ce111790cfb586ede6b6b
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-01 17:54:16 +0530

    URL Parameters - Blog Page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import articlesContent from "./articles-content";
 
-const Article = ({match}) => {
+const Article = ({ match }) => {
     const name = match.params.name;
+    const article = articlesContent.find((article) => article.name === name);
+    if(!article) return <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4 text-red-600">Article does not exisits</h1>
     return (
         <div className="mb-20">
-            <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">{name} Article</h1>
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">{article.title}</h1>
+            {article.content.map((paragraph, index) => (
+                <p className='max-auto leading-relaxed text-base mb-4' key='{index}'>{paragraph}</p>   
+            ))}
         </div>
     )
 }
---
+++ src/pages/articles-content.js
@@ -0,0 +1,45 @@
+const articles = [
+    {
+        name: 'learn-react',
+        title: 'Farstest way to Learn React',
+        content: [
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+        ]
+    },
+    {
+        name: 'learn-js',
+        title: 'Farstest way to Learn JavaScript',
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+        ]
+    },
+    {
+        name: 'learn-node',
+        title: 'Farstest way to Learn Node',
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+        ]
+    },
+    {
+        name: 'learn-ux',
+        title: 'Farstest way to Learn UX',
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+            'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
+        ]
+    },
+];
+export default articles;
