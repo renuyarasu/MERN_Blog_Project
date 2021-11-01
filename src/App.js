8748b5ff76663ce6bf80ffda23ae7cfebf093a7f
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-01 15:59:12 +0530

    Navbar Implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// Components
+
+// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ArticleList from "./pages/ArticleList";
 import Article from "./pages/Article";
 
+// Components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar/>
       <div className="max-w-screen-md mx-auto pt-20">
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
