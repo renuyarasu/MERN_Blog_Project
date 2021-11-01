6280dd50e1d258d51004cc506534ac1456d99a4a
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-01 20:43:59 +0530

    URL Parameters - Linking Articles list

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import articlesContent from "./articles-content";
 
 function ArticleList() {
     return (
         <div className="mb-20">
-        <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">Article List</h1>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-        <p className="max-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores porro, perferendis deserunt aliquam tenetur praesentium blanditiis minus. Distinctio quia voluptas voluptatibus quidem sunt quo?</p>
-    </div>
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900 mb-4">Article List</h1>
+            <div className="container py-4 mx-auto">
+                <div className="flex flex-wrap -m-4">
+                    {articlesContent.map((article, index) => (
+                        <div key={index} className='p-4 md:w-1/2'>
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hiddlen">
+                                <Link to={`/article/${article.name}`}>
+                                    <img src={article.thumbnail} alt="blog" className='lg:h-48 md:h-36 w-full object-cover object-center' />
+                                </Link>
+                                <div className="p-6">
+                                    <Link to={`/article/${article.name}`}>
+                                        <h3 className="title-font font-medium text-lg text-gray-900 mb-3">
+                                            {article.title}
+                                        </h3>
+                                    </Link>
+                                    <p>{article.content[0].substring(0, 100)}...</p>
+                                    <div className="flex item-center flex-wrap">
+                                        <Link className='text-indigo-500 inline-flex item-center md:mb-2 lg:mb-0' to={`/article/${article.name}`}>
+                                            Learn More...
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/articles-content.js
+++ src/pages/articles-content.js
@@ -2,6 +2,7 @@ const articles = [
     {
         name: 'learn-react',
         title: 'Farstest way to Learn React',
+        thumbnail: '/images/01.jpg',
         content: [
             'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
             'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
@@ -12,6 +13,7 @@ const articles = [
     {
         name: 'learn-js',
         title: 'Farstest way to Learn JavaScript',
+        thumbnail: '/images/02.jpg',
         content: [
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
             'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
@@ -22,6 +24,7 @@ const articles = [
     {
         name: 'learn-node',
         title: 'Farstest way to Learn Node',
+        thumbnail: '/images/03.jpg',
         content: [
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
             'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
@@ -33,6 +36,7 @@ const articles = [
     {
         name: 'learn-ux',
         title: 'Farstest way to Learn UX',
+        thumbnail: '/images/04.jpg',
         content: [
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
             'Learn React Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat odio beatae nesciunt natus nobis fugit accusantium, placeat perferendis corporis.',
